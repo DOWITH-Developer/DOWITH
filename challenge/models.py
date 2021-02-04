e90cbd0fb158304d3e04e95d2683fc4fd0437f5b
yb5464 <kyb3634@naver.com>
2021-02-04 17:57:17 +0900

    model setting

--- challenge/models.py
+++ challenge/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from login.models import User
 
 # Create your models here.
+class Challenge(models.Model):
+    CATEGORY_OF_CHALLENGE = (
+        ('all', '전체'),
+        ('language', '어학'),
+        ('job', '취업'),
+        ('NCS', '고시/공무원'),
+        ('programming', '프로그래밍'),
+        ('certificate', '자격증'),
+        ('other', '기타')
+    )
+
+    PRIVATE_OF_CHALLENGE = (
+        (0, '전체 공개'),
+        (1, '친구 공개'),
+        (2, '나만 보기'),
+    )
+
+    STATUS_OF_CHALLENGE = (
+        (0, '대기 중'),
+        (1, '진행 중'),
+        (2, '완료'), 
+    )
+
+    title = models.CharField(max_length=100, verbose_name="제목")
+    desc = models.TextField(verbose_name="설명")
+    category = models.CharField(max_length=100, choices=CATEGORY_OF_CHALLENGE, verbose_name="분류")
+    private = models.IntegerField(choices=PRIVATE_OF_CHALLENGE, verbose_name="공개 상태")
+    status = models.IntegerField(choices=STATUS_OF_CHALLENGE, verbose_name="진행 상태")
+    min_pp = models.PositiveIntegerField(verbose_name="최소 인원")
+    max_pp = models.PositiveIntegerField(verbose_name="최대 인원")
+    duration = models.PositiveIntegerField(verbose_name="기간")
+    start_date = models.DateField(verbose_name="시작일")
+    created_date = models.DateField(auto_now_add=True, verbose_name="생성일")
+
+class Enrollment(models.Model):
+    challenge = models.ForeignKey(Challenge, on_delete=models.CASCADE, verbose_name="챌린지", related_name="challenge_set")
+    player = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="유저", related_name="player_set")
--- friend/models.py
+++ friend/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from login.models import User
 
 # Create your models here.
+class Friendship(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="creator_set")
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name="follower_set")
