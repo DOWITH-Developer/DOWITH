d6defa871f50ec143c4e3ce3e3b44072679490aa
yb5464 <kyb3634@naver.com>
2021-02-04 18:03:02 +0900

    setting admin

--- challenge/admin.py
+++ challenge/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Challenge, Enrollment
 
 # Register your models here.
+@admin.register(Challenge)
+class FriendshipAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Enrollment)
+class FriendshipAdmin(admin.ModelAdmin):
+    pass
