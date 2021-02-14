eb4a8c04b8884e501dddc502315566118c9b04b0
jm choi <527wlals@gmail.com>
2021-02-14 23:33:13 +0900

    media 경로 설정

--- config/settings.py
+++ config/settings.py
@@ -140,6 +140,10 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
+
 AUTH_USER_MODEL = 'login.User'
 
 
--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +27,4 @@ urlpatterns = [
     path('', lambda req:redirect('challenge:ch_list'), name="home"),
     path("accounts/", include("allauth.urls")),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
