6840e1a77b7ad8371177c40667e90989c0dbaff9
jm choi <527wlals@gmail.com>
2021-02-16 00:53:05 +0900

    static 변경

--- challenge/urls.py
+++ challenge/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('<int:pk>/', challenge_detail, name='challenge_detail'),
     path('create/', challenge_create, name='challenge_create'),
     path('delete/<int:pk>/', challenge_delete, name='challenge_delete'),
-    path('list/', view=ch_list, name='ch_list'),
+    path('list/', view=challenge_list, name='challenge_list'),
     path('calendar/', view=challenge_calendar, name='challenge_calendar'),
     path('enrollment/<int:pk>', challenge_enrollment, name="challenge_enrollment"),
     path('result_ajax/', ResultAjax.as_view(), name="result_ajax"),
