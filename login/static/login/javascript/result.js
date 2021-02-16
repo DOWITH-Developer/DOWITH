134575196e7503f5658dee9ed64f66c9b4d28efe
HoYoungChun <ho723@naver.com>
2021-02-16 14:15:49 +0900

    challenge success button

--- login/static/login/javascript/result.js
+++ login/static/login/javascript/result.js
@@ -11,13 +11,16 @@ const onClickResult = async (id) => {
 
 const modify = (id, result) => {
     const heart = document.querySelector(`.result-${id} i`);
+    const result_word = document.querySelector(`.result-${id} .result_word`);
     const like_con = document.querySelector(`.result-${id} .result__content`);
 
     if (result === false) {
         heart.className = "far fa-check-circle";
         like_con.innerHTML = "챌린지가 성공되면 체크박스를 눌러주세요!";
+        result_word.value = "성공시 클릭";
     } else {
         heart.className = "fas fa-check-circle";
         like_con.innerHTML = "챌린지를 성공처리하셨습니다!";
+        result_word.value = "챌린지 성공!";
     }
 };
