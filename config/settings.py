f76fde0d2c0778e5e0188e84e4cf570318a12dc0
HoYoungChun <ho723@naver.com>
2021-02-18 18:20:57 +0900

    static / delete

--- config/settings.py
+++ config/settings.py
@@ -138,7 +138,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'config', 'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
