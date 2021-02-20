b39e2399395ae6c334f4d45b83a8b4418e973e34
jm choi <527wlals@gmail.com>
2021-02-20 21:09:27 +0900

    modified validation - challenge_create
    분류와 공개 상태 입력하지 않을 시 에러 메세지 띄워줌 -> 약간 수정했지만, 이 부분은 낭비가 심해서 추후에 변경하면 좋을 듯

--- challenge/views.py
+++ challenge/views.py
@@ -158,12 +158,6 @@ def challenge_create(request):
 
             return redirect(f'/challenge/list/{challenge.pk}')
         else:
-            print(form.errors)
-            # for error in form.errors:
-            #     if error == "category":
-            #         error_category = error
-            #         print(error_category)
-            print(form.non_field_errors())
             error_pp = ""
             error_date = ""
 
