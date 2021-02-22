87e7b54aa6232884183d4c53ccc6ab315c8f9907
leeinsong <insong1216@gmail.com>
2021-02-22 18:33:47 +0900

    캘린더 오늘날짜 표시수정

--- friend/static/friend/javascript/friend_list.js
+++ friend/static/friend/javascript/friend_list.js
@@ -1,17 +1,15 @@
 const firstModal = document.querySelector(".firstModal");
 const firstContent = document.querySelector("firstContent");
 const firstModalContent = document.querySelector(".first-modal-content");
-let firstModalText = firstModalContent.firstElementChild.textContent;
 
 const secondModal = document.querySelector(".secondModal");
 const secondContent = document.querySelector(".secondContent");
 const secondModalContent = document.querySelector(".second-modal-content");
-let secondModalText = secondModalContent.firstElementChild.textContent;
 
 const thirdModal = document.querySelector(".thirdModal");
 const thirdContent = document.querySelector(".thirdContent");
 const thirdModalContent = document.querySelector(".third-modal-content");
-let thirdModalText = thirdContent.textContent;
+// let thirdModalText = thirdContent.textContent;
 
 const alertModal = document.querySelector(".alert__modal");
 const alertModalContainer = document.querySelector(".alert__modal__container");
